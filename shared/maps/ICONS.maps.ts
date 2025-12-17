8c4e7f13bee4f1c20151c1b9b57c0950fcf81719
Felipe Baptistella <vbf.digi@gmail.com>
2025-12-17 19:02:31 -0300

    :wrench: Inclusão de backend

--- shared/maps/ICONS.maps.ts
+++
@@ -1,7 +0,0 @@
-import { FileText, LucideIcon, Plus, Shield } from "lucide-react";
-
-export const ICONS: Record<string, LucideIcon> = {
-  plus: Plus,
-  "file-text": FileText,
-  shield: Shield,
-};
--- shared/utils/icons.utils.ts
+++
@@ -1,8 +0,0 @@
-import { LucideIcon } from "lucide-react";
-import { ICONS } from "../maps/ICONS.maps";
-
-type IconName = keyof typeof ICONS;
-
-export function getIconFromMap(icon: IconName): LucideIcon {
-  return ICONS[icon];
-}
